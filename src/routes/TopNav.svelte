a393d0d0992be0a5bffad40b82cb04ce00361ad4
Fast DMS Corey <corey.damocles@fastdms.com>
2024-06-17 12:18:58 -0700

    Update instructions, move to TopNav component

--- src/routes/TopNav.svelte
+++ src/routes/TopNav.svelte
@@ -1,29 +1,72 @@
 <script>
 	import Tooltip from '$lib/components/Tooltip.svelte'
+	import Modal from '$lib/components/Modal.svelte'
+	import Divider from '$elements/Divider.svelte'
 </script>
 
-<div class="nav_wrapper" data-testid="top-nav">
+<div class="nav_wrapper">
 	<h1>Svelte STEP Bro</h1>
-	<Tooltip content="View on GitHub">
-		<a
-			class="github-link"
-			rel="noreferrer noopener"
-			href="https://github.com/ubemacapuno/svelte-step-cad-viewer"
-			target="_blank"
-			aria-label="GitHub Repository"
-		>
-			<svg
-				class="github-icon"
-				fill="var(--grey_7)"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
+	<div class="flex">
+		<Tooltip content="Instructions">
+			<Modal>
+				<span slot="trigger">
+					<IconButton name="info" tooltipText="Info" />
+				</span>
+				<div slot="title">Instructions</div>
+				<div>
+					<H3 accent="primary">Loading a STEP File</H3>
+					<p>
+						Click "Import STEP" to upload your file (.stp or .step). Click "Load Demo" to load a
+						demo model.
+					</p>
+					<H3 accent="primary">Viewing and Navigating</H3>
+					<p>
+						Zoom: Use mouse wheel or pinch to zoom (on mobile).<br />
+						Rotate: Hold left mouse button and move to rotate, or use one finger to rotate (on mobile).<br
+						/>
+						Move: Hold right mouse button and move, press Shift/Ctrl/Command + left mouse button to move,
+						or use two fingers to move (on mobile).
+					</p>
+					<H3 accent="primary">Additional Controls</H3>
+					<p>
+						Reset Camera<br />Toggle Info<br />Toggle Mode
+						<br />Remove Model
+					</p>
+					<H3 accent="primary">Model Information (currently in metric)</H3>
+					<p>
+						Surface Area<br />Part Volume<br />Bounding Box
+					</p>
+					<H3 accent="primary">Notes</H3>
+					<p>
+						Currently supports .stp and .step formats.<br />
+					</p>
+				</div>
+			</Modal>
+		</Tooltip>
+
+		<Divider />
+
+		<Tooltip content="View on GitHub">
+			<a
+				class="github-link"
+				rel="noreferrer noopener"
+				href="https://github.com/ubemacapuno/svelte-step-cad-viewer"
+				target="_blank"
+				aria-label="GitHub Repository"
 			>
-				<path
-					d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.602-3.369-1.34-3.369-1.34-.455-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.003.07 1.532 1.03 1.532 1.03.891 1.529 2.341 1.089 2.912.833.091-.647.349-1.086.635-1.335-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.276.098-2.656 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 7.029c.85 0 1.7.114 2.5.334 1.909-1.294 2.747-1.025 2.747-1.025.547 1.38.202 2.403.099 2.656.64.698 1.027 1.59 1.027 2.682 0 3.841-2.337 4.687-4.564 4.934.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.741 0 .267.18.577.688.479C19.138 20.164 22 16.416 22 12c0-5.523-4.477-10-10-10z"
-				/>
-			</svg>
-		</a>
-	</Tooltip>
+				<svg
+					class="github-icon"
+					fill="var(--grey_7)"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.602-3.369-1.34-3.369-1.34-.455-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.003.07 1.532 1.03 1.532 1.03.891 1.529 2.341 1.089 2.912.833.091-.647.349-1.086.635-1.335-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.276.098-2.656 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 7.029c.85 0 1.7.114 2.5.334 1.909-1.294 2.747-1.025 2.747-1.025.547 1.38.202 2.403.099 2.656.64.698 1.027 1.59 1.027 2.682 0 3.841-2.337 4.687-4.564 4.934.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.741 0 .267.18.577.688.479C19.138 20.164 22 16.416 22 12c0-5.523-4.477-10-10-10z"
+					/>
+				</svg>
+			</a>
+		</Tooltip>
+	</div>
 </div>
 
 <style>
@@ -37,7 +80,7 @@
 		justify-content: space-between;
 		align-items: center;
 		gap: var(--gap_smallest);
-		padding: 0 var(--gap_smallest);
+		padding: 0 var(--gap);
 		border-bottom: 1px solid var(--line_color);
 		grid-area: topnav;
 	}
@@ -49,5 +92,12 @@
 	.github-icon {
 		width: 100%;
 		height: 100%;
+	}
+
+	.flex {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: var(--gap_smallest);
 	}
 </style>
